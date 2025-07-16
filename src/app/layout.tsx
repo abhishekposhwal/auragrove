8595fa2727f3a414ce80d3cb649e48812f1e4edf
Abhishek Poshwal <abhishek.poshwal987@gmail.com>
2025-07-16 12:14:55 +0000

    add some more margin on left and right side of the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,11 @@ export default function RootLayout({
         <CartProvider>
           <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8">{children}</main>
+            <main className="flex-grow">
+              <div className="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+                {children}
+              </div>
+            </main>
             <Footer />
           </div>
           <Toaster />
